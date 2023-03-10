ba7fdcc2de8d05be2073287d94e16e4538e07db0
Antoine Cardon <c.antoine97@gmail.com>
2023-03-10 22:09:47 +0100

    feature/test-with-valid-mail

--- tests/units_tests/test_login.py
+++ tests/units_tests/test_login.py
@@ -3,6 +3,19 @@ from server import app
 client = app.test_client()
 
 
+def test_login_page():
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_login_with_valid_mail():
+    email = "john@simplylift.co"
+    response = client.post("/showSummary", data={"email": email})
+
+    assert response.status_code == 200
+
+
 def test_login_with_bad_mail():
     email = "thismaildoesnotexist@gudlft.co"
     response = client.post("/showSummary", data={"email": email})
