b93be30d531fa2beaefe1c0624db058fb557cc79
Antoine Cardon <c.antoine97@gmail.com>
2023-03-10 22:46:49 +0100

    bug/book-closed-competition

--- tests/units_tests/test_booking.py
+++ tests/units_tests/test_booking.py
@@ -61,3 +61,13 @@ def test_booking_with_more_twelve_points():
     )
 
     assert "Vous ne pouvez pas réserver + de 12 places" in response.data.decode()
+
+
+""" TEST BOOKING COMPETITION : Past, actually... """
+
+
+def test_book_closed_competition():
+    response = client.get(
+        f"/book/{server.competitions[0]['name']}/{server.clubs[0]['name']}"
+    )
+    assert "Cette compétition est déjà passée" in response.data.decode()
